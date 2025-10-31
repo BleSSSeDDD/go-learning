e4823569c40e44eab710d6f10b4b5ce69112ac8c
BleSSSeDDD <111vanya112@gmail.com>
2025-10-31 22:36:21 +0300

    leetcode haystack-needle

--- main.go
+++ main.go
@@ -1,67 +1,26 @@
 package main
 
-import (
-	"fmt"
-	"sort"
-)
+import "fmt"
 
-func TwoSum(nums []int, target int) []int {
+func strStr(haystack string, needle string) int {
 
-	mapp := make(map[int]int)
-
-	result := make([]int, 2)
-	var exists bool
-
-	for i, num := range nums {
-		pair := target - num
-		result[0] = i
-		result[1], exists = mapp[pair]
-		mapp[num] = i
-		if exists {
-			return result
-		}
+	if len(needle) > len(haystack) {
+		return -1
 	}
 
-	return nil
-}
-
-func threeSum(nums []int) [][]int {
-	res := make([][]int, 0)
-
-	mapp := make(map[[3]int]struct{})
-
-	for i, num := range nums {
-		tmp1 := make([]int, 1)
-
-		tmp1[0] = i
-
-		tmp2 := TwoSum(nums[i+1:], -num)
-		if tmp2 != nil {
-			tmp2[0] += i + 1
-			tmp2[1] += i + 1
-		}
-
-		if tmp2 != nil {
-			tmp1 = append(tmp1, tmp2...)
-			sort.Ints(tmp1)
-			triple := [3]int{
-				nums[i],
-				nums[tmp2[0]],
-				nums[tmp2[1]],
-			}
-
-			if _, exists := mapp[triple]; !exists {
-				res = append(res, tmp1)
-				mapp[triple] = struct{}{}
-			} else {
-				fmt.Println("Уже есть ", triple)
+	for i := 0; i < len(haystack)-len(needle); i++ {
+		for j := 0; haystack[i+j] == needle[j]; j++ {
+			if j == len(needle)-1 {
+				fmt.Println(j)
+				return i
 			}
 		}
 	}
-	return res
+
+	return -1
 }
 
 func main() {
-	nums := []int{-4, -2, -2, -1, -1, 0, 1, 2, 2, 3, 4, 5}
-	fmt.Println(threeSum(nums))
+	a := strStr("mississippi", "issipi")
+	fmt.Println(a)
 }
