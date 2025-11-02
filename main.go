0247153fa8f690a2de9351e58b13df2f42eb3b54
BleSSSeDDD <111vanya112@gmail.com>
2025-11-02 12:06:01 +0300

    second doubler

--- main.go
+++ main.go
@@ -3,21 +3,24 @@ package main
 import (
 	"fmt"
 	"sync"
+	"time"
 )
 
 func writer() <-chan int {
 	ch := make(chan int)
-	wg := sync.WaitGroup{}
+	wg := &sync.WaitGroup{}
 	wg.Add(2)
 	go func() {
 		defer wg.Done()
-		for i := 0; i < 10; i++ {
+		for i := 0; i < 5; i++ {
+			fmt.Println("Writer 1 writes i: ", i)
 			ch <- i
 		}
 	}()
 	go func() {
 		defer wg.Done()
-		for i := 0; i > -10; i-- {
+		for i := 0; i > -5; i-- {
+			fmt.Println("Writer 2 writes i: ", i)
 			ch <- i
 		}
 	}()
@@ -31,21 +34,43 @@ func writer() <-chan int {
 
 func doubler(ch <-chan int) <-chan int {
 	ch1 := make(chan int)
+	wg := &sync.WaitGroup{}
+	wg.Add(2)
 	go func() {
+		defer wg.Done()
+		count := 0
 		for v := range ch {
+			count++
+			fmt.Printf("Doubler 1 received %d (total: %d)\n", v, count)
 			ch1 <- v * 2
 		}
+		fmt.Printf("Doubler 1 finished, processed %d values\n", count)
+	}()
+	go func() {
+		defer wg.Done()
+		count := 0
+		for v := range ch {
+			count++
+			fmt.Printf("Doubler 2 received %d (total: %d)\n", v, count)
+			ch1 <- v * 2
+		}
+		fmt.Printf("Doubler 2 finished, processed %d values\n", count)
+	}()
+	go func() {
+		wg.Wait()
 		close(ch1)
 	}()
+
 	return ch1
 }
 
 func reader(ch <-chan int) {
 	for i := range ch {
-		fmt.Println(i)
+		fmt.Println("Reader recieves: ", i)
 	}
 }
 
 func main() {
 	reader(doubler(writer()))
+	time.Sleep(2 * time.Second)
 }
