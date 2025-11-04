e0a6506f34b7df4dcd698b36179a62a07bdab281
BleSSSeDDD <111vanya112@gmail.com>
2025-11-04 15:06:16 +0300

    my workerpool with context

--- main.go
+++ main.go
@@ -16,6 +16,7 @@ type WorkerPool struct {
 }
 
 func (w WorkerPool) Start() {
+	fmt.Println("Начался w.Start()")
 	wg := &sync.WaitGroup{}
 
 	defer close(w.resultChan)
@@ -23,12 +24,20 @@ func (w WorkerPool) Start() {
 	for i := 0; i < w.numWorkers; i++ {
 		wg.Add(1)
 		go func(goroutineID int) {
-			for num := range w.tasksChan {
-				fmt.Println("Воркер ", goroutineID, " записал в канал с результатами ", w.workersFunc(num), " при исходном числе ", num)
-				fmt.Print("")
-				w.resultChan <- w.workersFunc(num)
+			defer wg.Done()
+			for {
+				select {
+				case <-w.ctx.Done():
+					fmt.Println("Выход из воркера", goroutineID, " по контексту")
+					return
+				case num, ok := <-w.tasksChan:
+					if !ok {
+						fmt.Println("Воркер ", goroutineID, " закончил работу")
+						return
+					}
+					w.resultChan <- w.workersFunc(num)
+				}
 			}
-			wg.Done()
 		}(i + 1)
 	}
 
@@ -44,38 +53,56 @@ func squared(num int) int {
 	return num * num
 }
 
-func generator() <-chan int {
+func generator(ctx context.Context) <-chan int {
 	ch := make(chan int)
 	go func() {
+		defer func() {
+			close(ch)
+			fmt.Println("Генератор закончил и закрыл канал")
+		}()
+
 		fmt.Println("Генератор начал работу")
-		for i := 1; i <= 10000; i++ {
-			ch <- i
+		for i := 1; i <= 1000; i++ {
+			select {
+			case <-ctx.Done():
+				fmt.Println("Выход из generator по контексту")
+				return
+			case ch <- i:
+				fmt.Println("Генератор отправил ", i, " в канал")
+			}
 		}
-		close(ch)
-		fmt.Println("Генератор закончил и закрыл канал")
 	}()
 	return ch
 }
 
-func reader(ch <-chan int) {
-	for i := range ch {
-		fmt.Printf("Прочитано значение %d\n", i)
-		fmt.Print("")
+func reader(ctx context.Context, ch <-chan int) {
+	defer fmt.Println("Чтение закончено, канал закрыт")
+	for {
+		select {
+		case <-ctx.Done():
+			fmt.Println("Выход из reader по контексту")
+			return
+		case num, exists := <-ch:
+			if !exists {
+				return
+			}
+			fmt.Printf("Прочитано значение %d\n", num)
+		}
 	}
-	fmt.Println("Чтение закончено, канал закрыт")
+
 }
 
 func main() {
-	ctx, cancel := context.WithCancel(context.Background())
+	ctx, cancel := context.WithTimeout(context.Background(), 20*time.Millisecond)
 	defer cancel()
 
 	start := time.Now()
 	results := make(chan int)
-	w := WorkerPool{ctx, 100, timesTwo, generator(), results}
+	w := WorkerPool{ctx, 10, timesTwo, generator(ctx), results}
 
 	go w.Start()
 
-	reader(results)
+	reader(ctx, results)
 
 	fmt.Println("Мейн закончился cпустя ", time.Since(start))
 }
