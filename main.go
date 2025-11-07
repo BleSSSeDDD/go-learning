207eb922b95f08ff1b05e3bfd6e595cc6751db4d
BleSSSeDDD <111vanya112@gmail.com>
2025-11-07 20:57:53 +0300

    isHappy

--- main.go
+++ main.go
@@ -2,48 +2,26 @@ package main
 
 import (
 	"fmt"
-	"sort"
 )
 
-func threeSum(nums []int) [][]int {
-	res := make([][]int, 0, 15)
-	sort.Ints(nums)
-	var s int
-	for i := 0; i < len(nums)-2; i++ {
-		l, r := i+1, len(nums)-1
-		if i > 0 && nums[i] == nums[i-1] {
-			continue
+func isHappy(n int) bool {
+	mapp := make(map[int]struct{})
+	var squaredDigits int
+	for n != 1 {
+		if _, exists := mapp[n]; exists {
+			return false
 		}
-		for l < r {
-			s = nums[l] + nums[r]
-			if s == -nums[i] {
-				res = append(res, []int{nums[i], nums[l], nums[r]})
-				l++
-				r--
-				for l < r && nums[l-1] == nums[l] {
-					l++
-				}
-				for r > l && nums[r+1] == nums[r] {
-					r--
-				}
-			} else if s > -nums[i] {
-				r--
-				for r > l && nums[r+1] == nums[r] {
-					r--
-				}
-			} else {
-				l++
-				for l < r && nums[l-1] == nums[l] {
-					l++
-				}
-			}
-
+		mapp[n] = struct{}{}
+		squaredDigits = 0
+		for n > 0 {
+			squaredDigits += (n % 10) * (n % 10)
+			n /= 10
 		}
+		n = squaredDigits
 	}
-	return res
+	return true
 }
 
 func main() {
-	a := []int{-100, 50, 50, 50, 50, 70, 30}
-	fmt.Println(threeSum(a))
+	fmt.Println(isHappy(2))
 }
