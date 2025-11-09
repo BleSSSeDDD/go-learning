9a1ff328990f21118ac6c595cfef1a794ca7cc3f
BleSSSeDDD <111vanya112@gmail.com>
2025-11-09 19:01:53 +0300

    search

--- main.go
+++ main.go
@@ -2,54 +2,22 @@ package main
 
 import (
 	"fmt"
-	"sort"
 )
 
-func fourSum(nums []int, target int) [][]int {
-	res := make([][]int, 0, 10)
-	sort.Ints(nums)
-	for i := 0; i < len(nums)-3; i++ {
-		if i != 0 && nums[i] == nums[i-1] {
-			continue
-		}
-		fmt.Println("i = ", i)
-		for j := i + 1; j < len(nums)-2; j++ {
-			if j != i+1 && nums[j] == nums[j-1] {
-				continue
-			}
-			fmt.Println("j = ", j)
-			l, r := j+1, len(nums)-1
-			for l < r {
-				fmt.Println("l = ", l, " r = ", r)
-				if nums[l]+nums[r]+nums[i]+nums[j] == target {
-					res = append(res, []int{nums[i], nums[j], nums[l], nums[r]})
-					r--
-					l++
-					for l < r && nums[l] == nums[l-1] {
-						l++
-					}
-					for r > l && nums[r] == nums[r+1] {
-						r--
-					}
-				} else if nums[l]+nums[r] > target-(nums[i]+nums[j]) {
-					r--
-					for r > l && nums[r] == nums[r+1] {
-						r--
-					}
-				} else {
-					l++
-					for l < r && nums[l] == nums[l-1] {
-						l++
-					}
-				}
-			}
+func searchInsert(nums []int, target int) int {
+	l, r := 0, len(nums)-1
+	for l < r {
+		fmt.Println(l, r)
+		if nums[l+(r-l)/2] < target {
+			l = l + (r-l)/2
+		} else {
+			r = l + (r-l)/2
 		}
 	}
-	return res
+	return l
 }
 
 func main() {
-	a := []int{-2, -1, 0, 0, 1, 2}
-
-	fmt.Println(fourSum(a, 0))
+	a := []int{1, 2, 3, 4, 24356}
+	fmt.Println(searchInsert(a, 4))
 }
