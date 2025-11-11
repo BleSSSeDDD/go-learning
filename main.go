4380de65b0456ba07e9ecb7a3bd6d43fd4b39721
BleSSSed <111vanya112@gmail.com>
2025-11-11 09:31:55 +0300

    tree creation

--- main.go
+++ main.go
@@ -1,42 +1,23 @@
 package main
 
-import (
-	"fmt"
-	"sort"
-)
+import "fmt"
 
-func abs(a int) int {
-	if a < 0 {
-		return -a
-	}
-	return a
-}
-
-func threeSumClosest(nums []int, target int) int {
-	sort.Ints(nums)
-	res := nums[0] + nums[1] + nums[2]
-	var sum int
-	for i := 0; i < len(nums)-2; i++ {
-		l, r := i+1, len(nums)-1
-		for l < r {
-			sum = nums[i] + nums[l] + nums[r]
-			if sum-target == 0 {
-				return target
-			}
-			if abs(sum-target) < abs(res-target) {
-				res = sum
-			}
-			if sum < target {
-				l++
-			} else {
-				r--
-			}
-		}
-	}
-	return res
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
 }
 
 func main() {
-	a := []int{-100, 50, 50, 50, 50, 70, 30}
-	fmt.Println(threeSumClosest(a, 228))
+	root := &TreeNode{Val: 1, Left: &TreeNode{Val: 2}, Right: &TreeNode{Val: 3}}
+
+	fmt.Println(*root, *root.Left, *root.Right)
+
+	fmt.Println("Структура дерева:")
+	fmt.Printf("Корень: %d\n", root.Val)
+	fmt.Printf("Левый ребенок: %d\n", root.Left.Val)
+	fmt.Printf("Правый ребенок: %d\n", root.Right.Val)
+
+	fmt.Printf("\nУзел %d - лист? %t (нет детей)\n", root.Left.Val, root.Left.Left == nil && root.Left.Right == nil)
+	fmt.Printf("Узел %d - лист? %t (нет детей)\n", root.Right.Val, root.Right.Left == nil && root.Right.Right == nil)
 }
