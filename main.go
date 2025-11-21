e065d0fff82e02112d2677a17bcd1db2deb2cbaa
BleSSSed <111vanya112@gmail.com>
2025-11-21 14:55:29 +0300

    countPalindromicSubsequence

--- main.go
+++ main.go
@@ -2,49 +2,47 @@ package main
 
 import (
 	"fmt"
-	"strconv"
-	"strings"
 )
 
-func exclusiveTime(n int, logs []string) []int {
-	res := make([]int, n)
-	s := stack{}
+func countPalindromicSubsequence(s string) int {
+	var res int
+	first := make([]int, 26)
+	last := make([]int, 26)
 
-	for i := 0; i < len(logs); i++ {
-		log := strings.Split(logs[i], ":")
-		tmp, _ := strconv.Atoi(log[2])
-		id, _ := strconv.Atoi(log[0])
+	for i := range first {
+		first[i], last[i] = -1, -1
+	}
 
-		if log[1] == "start" {
-			s.push(tmp)
-		} else {
-			timeOfFunc := tmp - s.pop()
-			res[id] = timeOfFunc
+	for i := 0; i < len(s); i++ {
+		if first[s[i]-'a'] == -1 {
+			first[s[i]-'a'] = i
+		}
+	}
+
+	for i := len(s) - 1; i >= 0; i-- {
+		if last[s[i]-'a'] == -1 {
+			last[s[i]-'a'] = i
+		}
+	}
+
+	for i := 0; i < len(first); i++ {
+		uniq := make(map[byte]struct{})
+		if first[i] == -1 || last[i] == -1 {
+			continue
+		}
+		for j := first[i] + 1; j < last[i]; j++ {
+			if _, inUse := uniq[s[j]]; !inUse {
+				uniq[s[j]] = struct{}{}
+				res++
+			}
 		}
 	}
 
 	return res
 }
 
-type stack struct {
-	buf []int
-}
-
-func (s *stack) pop() int {
-	tmp := s.buf[len(s.buf)-1]
-	s.buf = s.buf[:len(s.buf)-1]
-	fmt.Println("Pop " + strconv.Itoa(tmp))
-	return tmp
-}
-
-func (s *stack) push(a int) {
-	s.buf = append(s.buf, a)
-	fmt.Println("Push " + strconv.Itoa(a))
-}
-
 func main() {
-	a := []string{"0:start:0", "1:start:2", "1:end:5", "2:start:6", "2:end:7", "0:end:8"}
-	n := 3
-	res := exclusiveTime(n, a)
+	a := "qwqw"
+	res := countPalindromicSubsequence(a)
 	fmt.Println(res)
 }
