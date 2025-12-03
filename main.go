8eedf75980f043e16161a5c0bebefd306d116b55
BleSSSeDDD <111vanya112@gmail.com>
2025-12-03 10:13:40 +0300

    zero valus deepEqual

--- main.go
+++ main.go
@@ -2,49 +2,33 @@ package main
 
 import (
 	"fmt"
-	"strconv"
-	"strings"
+	"reflect"
 )
 
-func exclusiveTime(n int, logs []string) []int {
-	res := make([]int, n)
-	s := stack{}
-
-	for i := 0; i < len(logs); i++ {
-		log := strings.Split(logs[i], ":")
-		tmp, _ := strconv.Atoi(log[2])
-		id, _ := strconv.Atoi(log[0])
-
-		if log[1] == "start" {
-			s.push(tmp)
-		} else {
-			timeOfFunc := tmp - s.pop()
-			res[id] = timeOfFunc
-		}
-	}
-
-	return res
-}
-
-type stack struct {
-	buf []int
-}
-
-func (s *stack) pop() int {
-	tmp := s.buf[len(s.buf)-1]
-	s.buf = s.buf[:len(s.buf)-1]
-	fmt.Println("Pop " + strconv.Itoa(tmp))
-	return tmp
-}
-
-func (s *stack) push(a int) {
-	s.buf = append(s.buf, a)
-	fmt.Println("Push " + strconv.Itoa(a))
+// Это та самая функция IsZeroValue из сгенерированного кода
+func IsZeroValue(val interface{}) bool {
+	return val == nil || reflect.DeepEqual(val, reflect.Zero(reflect.TypeOf(val)).Interface())
 }
 
 func main() {
-	a := []string{"0:start:0", "1:start:2", "1:end:5", "2:start:6", "2:end:7", "0:end:8"}
-	n := 3
-	res := exclusiveTime(n, a)
-	fmt.Println(res)
+	// Симулируем проблему которая была в коде
+	var fals bool = false
+	var tru bool = true
+	var str string = ""
+	var num int = 0
+
+	fmt.Println("=== Zero Value проверки ===")
+	fmt.Printf("false: IsZeroValue(%v) = %v\n", fals, IsZeroValue(fals))
+	fmt.Printf("true:  IsZeroValue(%v) = %v\n", tru, IsZeroValue(tru))
+	fmt.Printf("string \"\": IsZeroValue(%q) = %v\n", str, IsZeroValue(str))
+	fmt.Printf("int 0: IsZeroValue(%v) = %v\n", num, IsZeroValue(num))
+
+	fmt.Println("\n=== Что считалось 'zero value' ===")
+	fmt.Printf("reflect.Zero(reflect.TypeOf(false)) = %v\n", reflect.Zero(reflect.TypeOf(fals)).Interface())
+	fmt.Printf("reflect.Zero(reflect.TypeOf(true)) = %v\n", reflect.Zero(reflect.TypeOf(tru)).Interface())
+
+	fmt.Println("\n=== Проблема в валидации ===")
+	fmt.Println("Для required поля 'is_active':")
+	fmt.Println("- false → IsZeroValue(false) = true → ОШИБКА 'required field is zero value'")
+	fmt.Println("- true  → IsZeroValue(true) = false → УСПЕХ")
 }
