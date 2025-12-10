dbabb6d72206b9db28cdbeb37b12b984c31a18ef
BleSSSeDDD <111vanya112@gmail.com>
2025-12-10 18:36:39 +0300

    sort.Slice оч круто что такое есть

--- main.go
+++ main.go
@@ -2,47 +2,16 @@ package main
 
 import (
 	"fmt"
+	"sort"
 )
 
-func countPalindromicSubsequence(s string) int {
-	var res int
-	first := make([]int, 26)
-	last := make([]int, 26)
-
-	for i := range first {
-		first[i], last[i] = -1, -1
-	}
-
-	for i := 0; i < len(s); i++ {
-		if first[s[i]-'a'] == -1 {
-			first[s[i]-'a'] = i
-		}
-	}
-
-	for i := len(s) - 1; i >= 0; i-- {
-		if last[s[i]-'a'] == -1 {
-			last[s[i]-'a'] = i
-		}
-	}
-
-	for i := 0; i < len(first); i++ {
-		uniq := make(map[byte]struct{})
-		if first[i] == -1 || last[i] == -1 {
-			continue
-		}
-		for j := first[i] + 1; j < last[i]; j++ {
-			if _, inUse := uniq[s[j]]; !inUse {
-				uniq[s[j]] = struct{}{}
-				res++
-			}
-		}
-	}
-
-	return res
-}
-
 func main() {
-	a := "qwqw"
-	res := countPalindromicSubsequence(a)
-	fmt.Println(res)
+	a := [][]int{{20, 30}, {25, 10, 0}, {-10, 20}, {-10, 20}, {-10, 20}}
+	fmt.Println(a)
+
+	sort.Slice(a, func(i, j int) bool {
+		return a[i][0] < a[j][0]
+	})
+
+	fmt.Println(a)
 }
