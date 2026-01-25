a9a2aee97641751f889d25b57c30d0243db4540a
BleSSSeDDD <111vanya112@gmail.com>
2026-01-25 20:19:32 +0300

    q1wqe

--- main.go
+++ main.go
@@ -5,24 +5,8 @@ import (
 	"sort"
 )
 
-func binsearch(a []int, target int, l int, r int) int {
-	if l > r {
-		return -1
-	}
-
-	mid := l + (r-l)/2
-
-	if a[mid] == target {
-		return mid
-	} else if a[mid] < target {
-		return binsearch(a, target, mid+1, r)
-	}
-
-	return binsearch(a, target, l, mid-1)
-}
-
 func main() {
-	a := []int{}
-	sort.Ints(a)
-	fmt.Println(binsearch(a, 228, 0, len(a)-1))
+	arr := []int{9, 2, 34, 54, 53, 0}
+	sort.Ints(arr)
+	fmt.Println(arr)
 }
