dbec141c57fae96e62c5c114c99c2ef926d8f4b3
BleSSSeDDD <111vanya112@gmail.com>
2026-01-26 00:08:25 +0300

    file reading

--- main.go
+++ main.go
@@ -1,12 +1,40 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
-	"sort"
+	"os"
 )
 
 func main() {
-	arr := []int{9, 2, 34, 54, 53, 0}
-	sort.Ints(arr)
-	fmt.Println(arr)
+	f, err1 := os.Open("text.txt")
+	if err1 != nil {
+		fmt.Println(f, err1)
+	}
+
+	scanner := bufio.NewScanner(f)
+
+	scanner.Scan()
+
+	fmt.Println(scanner.Text())
+
+	defer f.Close()
+
+	fr, err2 := os.ReadFile("text.txt")
+	if err2 != nil {
+		fmt.Println(fr, err2)
+	}
+
+	a := 0
+	for _, c := range string(fr) {
+		if c == '\n' {
+			a++
+		}
+	}
+
+	if fr[len(fr)-1] != '\n' {
+		a++
+	}
+
+	fmt.Printf("Количество строк: %d", a)
 }
