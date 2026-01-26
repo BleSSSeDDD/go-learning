fdb0580642c6679ffb08ab294b0689e0acbc586b
BleSSSeDDD <111vanya112@gmail.com>
2026-01-26 18:18:25 +0300

    heap final

--- main.go
+++ main.go
@@ -1,10 +1,9 @@
 package main
 
-import "cmp"
-
-func main() {
-
-}
+import (
+	"cmp"
+	"fmt"
+)
 
 type Heap[T cmp.Ordered] struct {
 	buff  []T
@@ -19,102 +18,141 @@ func NewMaxHeap[T cmp.Ordered]() *Heap[T] {
 	return &Heap[T]{buff: make([]T, 0), isMin: false}
 }
 
-func compare[T cmp.Ordered](a, b T) bool {
-	return a > b
+// по сути дает ответ на вопрос с кем нам меняться: в случае minHeap возвращает большего, в случае maxHeap меньшего
+func compare[T cmp.Ordered](a, b T, isMin bool) bool {
+	if isMin {
+		return a > b
+	}
+	return b > a
 }
 
 func size[T cmp.Ordered](h *Heap[T]) int {
+	if h == nil {
+		return 0
+	}
 	return len(h.buff)
 }
 
 func appendToHeap[T cmp.Ordered](h *Heap[T], element T) {
 	h.buff = append(h.buff, element)
-	hippifyUp(h.buff, h.isMin)
+	hippifyUp(h)
 }
 
-func hippifyUp[T cmp.Ordered](buff []T, isMin bool) {
-	index := len(buff) - 1
-	if isMin {
-		for index > 0 && compare(buff[(index-1)/2], buff[index]) {
-			buff[(index-1)/2], buff[index] = buff[index], buff[(index-1)/2]
-			index = (index - 1) / 2
-		}
-	} else {
-		for index > 0 && !compare(buff[(index-1)/2], buff[index]) {
-			buff[(index-1)/2], buff[index] = buff[index], buff[(index-1)/2]
-			index = (index - 1) / 2
-		}
+func hippifyUp[T cmp.Ordered](h *Heap[T]) {
+	index := len(h.buff) - 1
+	for index > 0 && compare(h.buff[(index-1)/2], h.buff[index], h.isMin) {
+		h.buff[(index-1)/2], h.buff[index] = h.buff[index], h.buff[(index-1)/2]
+		index = (index - 1) / 2
 	}
 }
 
 func deleteFromHeap[T cmp.Ordered](h *Heap[T]) {
+	if h == nil || len(h.buff) == 0 {
+		return
+	}
 	h.buff[0], h.buff[len(h.buff)-1] = h.buff[len(h.buff)-1], h.buff[0]
 	h.buff = h.buff[0 : len(h.buff)-1]
-	hippifyDown(h.buff, h.isMin)
+	hippifyDown(h)
 }
 
-func hippifyDown[T cmp.Ordered](buff []T, isMin bool) {
+func hippifyDown[T cmp.Ordered](h *Heap[T]) {
 	index := 0
 
-	if isMin {
-		for {
-			left, right := index*2+1, index*2+2
-			if left < len(buff) && right < len(buff) {
-				if compare(buff[left], buff[right]) {
-					if compare(buff[index], buff[left]) {
-						buff[left], buff[index] = buff[index], buff[left]
-						index = left
-					} else {
-						break
-					}
-				} else {
-					if compare(buff[index], buff[right]) {
-						buff[right], buff[index] = buff[index], buff[right]
-						index = right
-					} else {
-						break
-					}
-				}
-			} else if left < len(buff) {
-				if compare(buff[index], buff[left]) {
-					buff[left], buff[index] = buff[index], buff[left]
-					index = left
-				} else {
-					break
-				}
-			} else {
-				break
-			}
+	for {
+		swapIndex := index
+		left, right := index*2+1, index*2+2
+		if left < len(h.buff) && compare(h.buff[index], h.buff[left], h.isMin) {
+			swapIndex = left
 		}
-	} else {
-		for {
-			left, right := index*2+1, index*2+2
-			if left < len(buff) && right < len(buff) {
-				if !compare(buff[left], buff[right]) {
-					if !compare(buff[index], buff[left]) {
-						buff[left], buff[index] = buff[index], buff[left]
-						index = left
-					} else {
-						break
-					}
-				} else {
-					if !compare(buff[index], buff[right]) {
-						buff[right], buff[index] = buff[index], buff[right]
-						index = right
-					} else {
-						break
-					}
-				}
-			} else if left < len(buff) {
-				if !compare(buff[index], buff[left]) {
-					buff[left], buff[index] = buff[index], buff[left]
-					index = left
-				} else {
-					break
-				}
-			} else {
-				break
-			}
+		if right < len(h.buff) && compare(h.buff[swapIndex], h.buff[right], h.isMin) {
+			swapIndex = right
 		}
+
+		if swapIndex == index {
+			break
+		}
+
+		h.buff[index], h.buff[swapIndex] = h.buff[swapIndex], h.buff[index]
+		index = swapIndex
 	}
+}
+
+func peek[T cmp.Ordered](h *Heap[T]) T {
+	if h == nil || len(h.buff) == 0 {
+		var zero T
+		return zero
+	}
+	return h.buff[0]
+}
+
+func pop[T cmp.Ordered](h *Heap[T]) T {
+	if h == nil || len(h.buff) == 0 {
+		var zero T
+		return zero
+	}
+	res := h.buff[0]
+	deleteFromHeap(h)
+	return res
+}
+
+func main() {
+	// --- Тест Min-Heap ---
+	fmt.Println("=== Min-Heap Test ===")
+	minHeap := NewMinHeap[int]()
+
+	fmt.Println("Добавляем элементы: 5, 3, 8, 1, 6")
+	appendToHeap(minHeap, 5)
+	appendToHeap(minHeap, 3)
+	appendToHeap(minHeap, 8)
+	appendToHeap(minHeap, 1)
+	appendToHeap(minHeap, 6)
+
+	fmt.Println("Размер кучи:", size(minHeap))
+	fmt.Println("Peek:", peek(minHeap))
+
+	fmt.Println("Pop элементы:")
+	for size(minHeap) > 0 {
+		fmt.Println(pop(minHeap))
+	}
+
+	// --- Тест Max-Heap ---
+	fmt.Println("\n=== Max-Heap Test ===")
+	maxHeap := NewMaxHeap[int]()
+
+	fmt.Println("Добавляем элементы: 5, 3, 8, 1, 6")
+	appendToHeap(maxHeap, 5)
+	appendToHeap(maxHeap, 3)
+	appendToHeap(maxHeap, 8)
+	appendToHeap(maxHeap, 1)
+	appendToHeap(maxHeap, 6)
+
+	fmt.Println("Размер кучи:", size(maxHeap))
+	fmt.Println("Peek:", peek(maxHeap))
+
+	fmt.Println("Pop элементы:")
+	for size(maxHeap) > 0 {
+		fmt.Println(pop(maxHeap))
+	}
+
+	fmt.Println("\n=== Max-Heap Stress Test (случайные данные) ===")
+	stressHeap := NewMaxHeap[int]()
+	testData := []int{100, -5, 42, 777, 0, 23, -100, 500, 1, 1, 1, 999}
+
+	fmt.Println("Вставляем в порядке:", testData)
+	for _, v := range testData {
+		appendToHeap(stressHeap, v)
+	}
+
+	fmt.Println("Извлекаем всё (должно быть по убыванию):")
+	prev := peek(stressHeap)
+	for size(stressHeap) > 0 {
+		curr := pop(stressHeap)
+		fmt.Printf("%d ", curr)
+		// Проверка свойства max-heap: каждый следующий извлечённый должен быть <= предыдущего
+		if curr > prev {
+			fmt.Printf("\n⛔ ОШИБКА: нарушение порядка! %d > %d\n", curr, prev)
+		}
+		prev = curr
+	}
+	fmt.Println("\n✅ Стресс-тест пройден, если выше нет ошибок.")
 }
