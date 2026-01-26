95b3c2fe975f84e8d7e3dc02d01f85d583486688
BleSSSeDDD <111vanya112@gmail.com>
2026-01-26 16:54:42 +0300

    heap

--- main.go
+++ main.go
@@ -1,40 +1,120 @@
 package main
 
-import (
-	"bufio"
-	"fmt"
-	"os"
-)
+import "cmp"
 
 func main() {
-	f, err1 := os.Open("text.txt")
-	if err1 != nil {
-		fmt.Println(f, err1)
-	}
 
-	scanner := bufio.NewScanner(f)
+}
 
-	scanner.Scan()
+type Heap[T cmp.Ordered] struct {
+	buff  []T
+	isMin bool
+}
 
-	fmt.Println(scanner.Text())
+func NewMinHeap[T cmp.Ordered]() *Heap[T] {
+	return &Heap[T]{buff: make([]T, 0), isMin: true}
+}
 
-	defer f.Close()
+func NewMaxHeap[T cmp.Ordered]() *Heap[T] {
+	return &Heap[T]{buff: make([]T, 0), isMin: false}
+}
 
-	fr, err2 := os.ReadFile("text.txt")
-	if err2 != nil {
-		fmt.Println(fr, err2)
-	}
+func compare[T cmp.Ordered](a, b T) bool {
+	return a > b
+}
 
-	a := 0
-	for _, c := range string(fr) {
-		if c == '\n' {
-			a++
+func size[T cmp.Ordered](h *Heap[T]) int {
+	return len(h.buff)
+}
+
+func appendToHeap[T cmp.Ordered](h *Heap[T], element T) {
+	h.buff = append(h.buff, element)
+	hippifyUp(h.buff, h.isMin)
+}
+
+func hippifyUp[T cmp.Ordered](buff []T, isMin bool) {
+	index := len(buff) - 1
+	if isMin {
+		for index > 0 && compare(buff[(index-1)/2], buff[index]) {
+			buff[(index-1)/2], buff[index] = buff[index], buff[(index-1)/2]
+			index = (index - 1) / 2
+		}
+	} else {
+		for index > 0 && !compare(buff[(index-1)/2], buff[index]) {
+			buff[(index-1)/2], buff[index] = buff[index], buff[(index-1)/2]
+			index = (index - 1) / 2
 		}
 	}
+}
 
-	if fr[len(fr)-1] != '\n' {
-		a++
+func deleteFromHeap[T cmp.Ordered](h *Heap[T]) {
+	h.buff[0], h.buff[len(h.buff)-1] = h.buff[len(h.buff)-1], h.buff[0]
+	h.buff = h.buff[0 : len(h.buff)-1]
+	hippifyDown(h.buff, h.isMin)
+}
+
+func hippifyDown[T cmp.Ordered](buff []T, isMin bool) {
+	index := 0
+
+	if isMin {
+		for {
+			left, right := index*2+1, index*2+2
+			if left < len(buff) && right < len(buff) {
+				if compare(buff[left], buff[right]) {
+					if compare(buff[index], buff[left]) {
+						buff[left], buff[index] = buff[index], buff[left]
+						index = left
+					} else {
+						break
+					}
+				} else {
+					if compare(buff[index], buff[right]) {
+						buff[right], buff[index] = buff[index], buff[right]
+						index = right
+					} else {
+						break
+					}
+				}
+			} else if left < len(buff) {
+				if compare(buff[index], buff[left]) {
+					buff[left], buff[index] = buff[index], buff[left]
+					index = left
+				} else {
+					break
+				}
+			} else {
+				break
+			}
+		}
+	} else {
+		for {
+			left, right := index*2+1, index*2+2
+			if left < len(buff) && right < len(buff) {
+				if !compare(buff[left], buff[right]) {
+					if !compare(buff[index], buff[left]) {
+						buff[left], buff[index] = buff[index], buff[left]
+						index = left
+					} else {
+						break
+					}
+				} else {
+					if !compare(buff[index], buff[right]) {
+						buff[right], buff[index] = buff[index], buff[right]
+						index = right
+					} else {
+						break
+					}
+				}
+			} else if left < len(buff) {
+				if !compare(buff[index], buff[left]) {
+					buff[left], buff[index] = buff[index], buff[left]
+					index = left
+				} else {
+					break
+				}
+			} else {
+				break
+			}
+		}
 	}
-
-	fmt.Printf("Количество строк: %d", a)
 }
