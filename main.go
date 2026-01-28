cf65b458c77a4778ea1cdc9f9abccfe082d5c091
BleSSSeDDD <111vanya112@gmail.com>
2026-01-28 15:13:36 +0300

    BFS

--- main.go
+++ main.go
@@ -1,14 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
-	a := &TreeNode{Val: "a"}
-	b := &TreeNode{Val: "b"}
-	c := &TreeNode{Val: "c"}
-	d := &TreeNode{Val: "d"}
-	e := &TreeNode{Val: "e"}
-	f := &TreeNode{Val: "f"}
+	a := &TreeNode[string]{Val: "a"}
+	b := &TreeNode[string]{Val: "b"}
+	c := &TreeNode[string]{Val: "c"}
+	d := &TreeNode[string]{Val: "d"}
+	e := &TreeNode[string]{Val: "e"}
+	f := &TreeNode[string]{Val: "f"}
 
 	a.Left = b
 	a.Right = c
@@ -16,16 +18,56 @@ func main() {
 	b.Right = e
 	c.Right = f
 
-	fmt.Println(a.DFS())
+	fmt.Println(a.IterativeDFS())
+	fmt.Println(a.RecursiveDFS())
+	fmt.Println(a.BFS())
+
 }
 
-func (t *TreeNode) DFS() []string {
+func (t *TreeNode[T]) BFS() []T {
+	res := make([]T, 0)
+
+	q := Queue[*TreeNode[T]]{}
+	q.Push(t)
+
+	for !q.Empty() {
+		curr := q.Pop()
+
+		res = append(res, curr.Val)
+
+		if curr.Left != nil {
+			q.Push(curr.Left)
+		}
+
+		if curr.Right != nil {
+			q.Push(curr.Right)
+		}
+	}
+
+	return res
+}
+
+func (t *TreeNode[T]) RecursiveDFS() []T {
+	res := make([]T, 0)
+
+	if t == nil {
+		return []T{}
+	}
+
+	res = append(res, t.Val)
+	res = append(res, t.Left.RecursiveDFS()...)
+	res = append(res, t.Right.RecursiveDFS()...)
+
+	return res
+}
+
+func (t *TreeNode[T]) IterativeDFS() []T {
 	if t == nil {
 		return nil
 	}
 
-	res := make([]string, 0)
-	s := Stack[*TreeNode]{}
+	res := make([]T, 0)
+	s := Stack[*TreeNode[T]]{}
 
 	s.Push(t)
 
@@ -45,10 +87,10 @@ func (t *TreeNode) DFS() []string {
 	return res
 }
 
-type TreeNode struct {
-	Val   string
-	Right *TreeNode
-	Left  *TreeNode
+type TreeNode[T any] struct {
+	Val   T
+	Right *TreeNode[T]
+	Left  *TreeNode[T]
 }
 
 func (s *Stack[T]) Empty() bool {
@@ -71,4 +113,26 @@ func (s *Stack[T]) Pop() T {
 
 func (s *Stack[T]) Push(a T) {
 	s.buff = append(s.buff, a)
+}
+
+type Queue[T any] struct {
+	buff []T
+}
+
+func (q *Queue[T]) Push(a T) {
+	q.buff = append(q.buff, a)
+}
+
+func (q *Queue[T]) Pop() T {
+	var zero T
+	if q.Empty() {
+		return zero
+	}
+	res := q.buff[0]
+	q.buff = q.buff[1:]
+	return res
+}
+
+func (q *Queue[T]) Empty() bool {
+	return len(q.buff) == 0
 }
